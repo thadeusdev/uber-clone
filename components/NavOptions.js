a664598e3f684da07a82db7fcec89824e92fcf70
thadeus <thadeusnyariki94@gmail.com>
2023-01-23 02:55:11 +0300

    added navigator

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,8 +2,10 @@ import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
 import { Icon } from 'react-native-elements/dist/icons/Icon';
+import { useNavigation } from '@react-navigation/native'
 
 const NavOptions = () => {
+    const navigation = useNavigation();
 
     const data = [
         {
@@ -25,7 +27,10 @@ const NavOptions = () => {
         horizontal
         keyExtractor={(item) => item.id}
         renderItem={({item}) => (
-            <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>                
+            <TouchableOpacity 
+                onPress={() => navigation.navigate(item.screen)}
+                style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+            >               
                 <View>
                     <Image 
                         style={{width:120, height:120, resizeMode: 'contain'}}

41d3df992769af7cd388969396cbf110879380b6
thadeus <thadeusnyariki94@gmail.com>
2023-01-22 23:09:31 +0300

    provider done

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import {store} from './store';
 
 // 1) set up redux
 
 export default function App() {
   return (
-    <Provider>
+    <Provider store={store}>
       <View style={styles.container}>
         <Text>Lets build UBER!</Text>
       </View>

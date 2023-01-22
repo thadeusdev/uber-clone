a2b4a5a3956301e674522fcd6b0e868affd15250
thadeus <thadeusnyariki94@gmail.com>
2023-01-23 02:21:42 +0300

    created navOptions

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
 import {store} from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 // 1) set up redux
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Lets build UBER!</Text>
-      </View>
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>     
     </Provider>
   );
 }

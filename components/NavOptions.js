5e318b8f5105d6729b12391188654884cb1d9333
thadeus <thadeusnyariki94@gmail.com>
2023-01-23 14:36:25 +0300

    added location

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -3,9 +3,12 @@ import React from 'react'
 import tw from 'tailwind-react-native-classnames';
 import { Icon } from 'react-native-elements/dist/icons/Icon';
 import { useNavigation } from '@react-navigation/native'
+import { useSelector } from 'react-redux';
+import { selectOrigin } from '../slices/navSlice';
 
 const NavOptions = () => {
     const navigation = useNavigation();
+    const origin = useSelector(selectOrigin)
 
     const data = [
         {
@@ -30,8 +33,9 @@ const NavOptions = () => {
             <TouchableOpacity 
                 onPress={() => navigation.navigate(item.screen)}
                 style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+                disabled={!origin}
             >               
-                <View>
+                <View style={tw`${!origin && 'opacity-20'}`}>
                     <Image 
                         style={{width:120, height:120, resizeMode: 'contain'}}
                         source={{ uri: item.image }}
